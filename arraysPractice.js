6b8ca95c510ecf6b42e5571c5f7c34a88d89df95
J. A. Kohler <Julie@Julies-MBP.home>
2017-01-08 22:42:00 -0700

    all but 3 exercises done

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,9 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
-  //Code Here
+function first(arr) {
+  return arr[0];
+}
 
 
 //Next problem
@@ -15,8 +17,9 @@ var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
 
 
-  //Code Here
-
+function last (arr) {
+  return arr[arr.length - 1];
+}
 
 //Next Problem
 
@@ -24,9 +27,13 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+function looper(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    alert(arr[i]);
+  }
+}
 
-
+looper(family);
 //Next problem
 
 
@@ -34,7 +41,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
-  //Code Here
+function reversedLooper(arr) {
+  for (var i = arr.length - 1; i > -1; i--) {
+    alert(arr[i]);
+  }
+}
+
+reversedLooper(letters);
 
 
 //Next Problem
@@ -43,7 +56,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
-  //Code Here
+function evenFinder(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 !== 0) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums;
+}
 
 
 //Next problem
@@ -53,9 +73,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 //Write a function called divider that is given one argument, numbersArray.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
-
-
-  //Code Here
+function divider(numbersArray) {
+  var evens = [];
+  var odds = [];
+  var fullArr = [];
+  for (var i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] % 2 === 0) {
+      evens.push(numbersArray[i]);
+    } 
+    else {
+      odds.push(numbersArray[i]);
+    }
+  }
+  fullArr.push(evens, odds);
+  return fullArr;
+} 
 
 
 //Next Problem
@@ -69,9 +101,14 @@ var getRandomArbitrary = function() {
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
-
-  //Code Here
+function finder(arr) {
+  if (arr.indexOf(getRandomArbitrary()) > -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 
 //Next problem
@@ -81,7 +118,10 @@ var getRandomArbitrary = function() {
 var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
-  //Code Here
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
 
 
 //Next Problem
@@ -116,7 +156,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+function maker() {
+  var arr = [];
+  for (var i = 1; i <= 215; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
 
 
@@ -127,9 +173,13 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
-  //Code Here
-
-
+function addTen(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(parseInt(arr[i], 10) + 10);
+  }
+  return newArr;
+}
 
 //Next Problem
 
@@ -148,7 +198,14 @@ for(var i = 0; i < num2; i++){
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+function longer(arr1, arr2) {
+  if (arr1 > arr2) {
+    return arr1;
+  }
+  else {
+    return arr2;
+  }
+}
 
 
 /*As a continuation of the previous problem, write another function called 'both'.
@@ -157,7 +214,17 @@ for(var i = 0; i < num2; i++){
   Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-  //Code Here
+function both(arr1, arr2) {
+  var sharedValues = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        sharedValues.push(arr1[i]);
+      }
+    }
+  }
+  return sharedValues;
+}
 
 
 
@@ -193,16 +260,26 @@ var colt = {
     spiritAnimal: 'Young Male Horse'
 };
 
-/*Above you're given an empty array with four objects. Fill the devMountainEmployees
+/*Above you're given an empty array and four objects. Fill the devMountainEmployees
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+devMountainEmployees.push(tyler, cahlan, ryan, colt);
+console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+function removal(arr, absentee) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].name === absentee) {
+      arr.splice(i, 1);
+    }
+  }
+  return devMountainEmployees;
+}
+
+removal(devMountainEmployees, "Cahlan");
 
 
 
